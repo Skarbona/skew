130db884d8adc1e57a08c27d4b132735a01bfebe
Filip <filip.sokolowski1990@gmail.com>
2018-05-10 21:36:30 +0200

    add mobile support

--- js/main.js
+++ js/main.js
@@ -20,5 +20,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 
+    wrapper.addEventListener('touchmove', forMouse, true);
+    wrapper.addEventListener('touchstart', forMouse, true);
+    wrapper.addEventListener('touchend', forMouse, true);
+    wrapper.addEventListener('touchcancel', forMouse, true);
+
+    function forMouse(e){
+        console.log(e.touches[0].clientX);
+        delta = (e.touches[0].clientX - window.innerWidth / 2) * 0.5;
+
+        handle.style.left = e.touches[0].clientX + delta + 'px';
+        topLayer.style.width = e.touches[0].clientX + skew + delta + 'px';
+
+    }
+
+
 });
 
